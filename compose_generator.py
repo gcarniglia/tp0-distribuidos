ff510967243116bfe8c2ccadf84918f0ce6d8dfb
gabyga-11 <gabycarniglia@gmail.com>
2026-03-22 19:37:37 -0300

    use yaml library to improve yaml writing

--- compose_generator.py
+++ compose_generator.py
@@ -1,52 +1,58 @@
 
 from sys import argv
+import yaml
 
 CANTIDAD_PARAMETROS = 2
 NOMBRE_SUBSCRIPT = "compose_generator"
 
+
+class IndentDumper(yaml.SafeDumper):
+    # Hace que las listas queden indentadas debajo de su clave (estilo docker-compose).
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+
 def generar_compose(nombre_archivo, cantidad_clientes):
-    # Definición fija del servidor
-    contenido = """services:
-                server:
-                    container_name: server
-                    image: server:latest
-                    entrypoint: python3 /main.py
-                    environment:
-                    - PYTHONUNBUFFERED=1
-                    - LOGGING_LEVEL=DEBUG
-                    networks:
-                    - testing_net
-                """
+    data = {
+        # Mantiene el mismo formato que el ejemplo provisto en docker-compose-dev.yaml
+        "name": "tp0",
+        "services": {
+            "server": {
+                "container_name": "server",
+                "image": "server:latest",
+                "entrypoint": "python3 /main.py",
+                "environment": ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"],
+                "networks": ["testing_net"],
+            }
+        },
+        "networks": {
+            "testing_net": {
+                "ipam": {
+                    "driver": "default",
+                    "config": [{"subnet": "172.25.125.0/24"}],
+                }
+            }
+        },
+    }
 
-    # Generar dinámicamente los clientes
     for i in range(1, cantidad_clientes + 1):
-        contenido += f"""
-        client{i}:
-            container_name: client{i}
-            image: client:latest
-            entrypoint: /client
-            environment:
-            - CLI_ID={i}
-            - CLI_LOG_LEVEL=DEBUG
-            networks:
-            - testing_net
-            depends_on:
-            - server
-        """
+        data["services"][f"client{i}"] = {
+            "container_name": f"client{i}",
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [f"CLI_ID={i}", "CLI_LOG_LEVEL=DEBUG"],
+            "networks": ["testing_net"],
+            "depends_on": ["server"],
+        }
 
-    # Definición de la red
-    contenido += """
-        networks:
-        testing_net:
-            ipam:
-            driver: default
-            config:
-                - subnet: 172.25.125.0/24
-        """
-
-    # Escribir el contenido en el archivo
-    with open(nombre_archivo, 'w') as archivo:
-        archivo.write(contenido)
+    with open(nombre_archivo, "w") as archivo:
+        yaml.dump(
+            data,
+            archivo,
+            Dumper=IndentDumper,
+            sort_keys=False,
+            default_flow_style=False,
+            indent=2,
+        )
 
 def validate_parameters(argv):
     if len(argv) != CANTIDAD_PARAMETROS+1:

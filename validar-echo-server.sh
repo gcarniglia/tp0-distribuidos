b50b472c3f2d518443d7a8dc0929e280654ac7f7
gabyga-11 <gabycarniglia@gmail.com>
2026-03-23 23:14:15 -0300

    add fix of sh call in .sh file

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MSG="hola mundo"
+MSG="hola"
 
-RESPONSE=$(docker compose -f docker-compose-dev.yaml run --rm -T tester sh -c "echo '$MSG' | nc server 12345 2>/dev/null" | tr -d '\r\n')
+RESPONSE=$(docker compose -f docker-compose-dev.yaml run --rm -T tester -c "echo '$MSG' | nc server 12345" 2>/dev/null | tr -d '\r\n')
 
 if [ "$RESPONSE" = "$MSG" ]; then
   echo "action: test_echo_server | result: success"
